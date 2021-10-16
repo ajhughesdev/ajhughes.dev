998a08e3510718fe6961a85c58405294663fab69
Andrew J Hughes <andrew@ajhughes.dev>
2021-10-16 14:52:28 -0400

    new desktop design implementation

---
+++ ajhughes.dev/js/main.js
@@ -0,0 +1,22 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+if (
+  "IntersectionObserver" in window &&
+  "IntersectionObserverEntry" in window &&
+  "intersectionRatio" in window.IntersectionObserverEntry.prototype
+) {
+  let observer = new IntersectionObserver(entries => {
+    if (entries[0].boundingClientRect.y < 0) {
+      document.body.classList.add("header-not-at-top");
+    } else {
+      document.body.classList.remove("header-not-at-top");
+    }
+  });
+  observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
+}
